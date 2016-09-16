c6b3b6ba3b319d5a1d65c4f194b4efbc6a0dbea1
Steve Arnett <steve@sheetmusicplus.com>
2016-09-16 16:01:59 -0700

    top and bottom nav

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-kill $(ps aux | grep '[g]unicorn' | awk '{print $2}');
-sudo /etc/init.d/nginx restart;
-gunicorn app:app --bind 0.0.0.0:5000 --workers 16 &;
+kill $(ps aux | grep '[g]unicorn' | awk '{print $2}')
+
+sudo /etc/init.d/nginx restart
+
+gunicorn --bind 0.0.0.0:5000 --workers 16 wsgi &
