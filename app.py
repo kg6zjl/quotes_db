7316e7b194c7c4f1bcc8a874500a2182265a9986
Steve Arnett <steve@sheetmusicplus.com>
2016-09-16 10:43:34 -0700

    switch to mysql

--- app.py
+++ app.py
@@ -3,34 +3,28 @@
 import os
 from flask import Flask, render_template, request, json, abort, make_response, jsonify
 from werkzeug import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
+try:
+	from flask.ext.mysql import MySQL
+except:
+	from flask_mysql import MySQL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////quotes.db'
-db = SQLAlchemy(app)
+mysql = MySQL()
 
-class Quote(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	quote = db.Column(db.String(255), unique=True)
-	contributor = db.Column(db.String(120))
-	private = db.Column(db.String(5))
-	created_at = db.Column(db.String(120))
-	updated_at = db.Column(db.String(120))
+# MySQL configurations/ENV Vars
+app.config['MYSQL_DATABASE_USER'] = os.environ['QUOTES_DB_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['QUOTES_DB_PASS']
+app.config['MYSQL_DATABASE_DB'] = os.environ['QUOTES_DB_NAME']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['QUOTES_DB_HOST']
+mysql.init_app(app)
 
-	def __init__(self, quote, contributor, private):
-		self.quote = quote 
-		self.contributor = contributor
-		self.private = private
-
-	def __repr__(self):
-		return '<Quote %r>' % self.quote
 
 
 @app.route("/")
 def main():
 	return render_template('index.html')
 
-@app.route("/submit")
+@app.route("/submitQuote")
 def submitRecipe():
 	return render_template('add_quote.html')
 
@@ -39,10 +33,10 @@ def thanks():
 	return render_template('thanks.html')
 
 @app.route("/recent")
-def recentRecipes():
+def recent():
 	conn = mysql.connect()
 	cursor = conn.cursor()
-	query = ("select * from ridiculous.quotes order by created_at DESC limit 10;")
+	query = ("select * from quotes.quotes order by id DESC limit 10;")
 	cursor.execute(query)
 	data = cursor.fetchall()
 	
@@ -51,8 +45,7 @@ def recentRecipes():
 @app.route('/addQuote',methods=['POST','GET'])
 def addRecipe(): # read the posted values from the UI
 	_quote = request.form['quoteText']
-	_description = request.form['quote']
-	_contributor = request.form['recipeContributor']
+	_author = request.form['quoteAuthor']
 
 	# validate the received values
 	if _title and _description and _contributor:
