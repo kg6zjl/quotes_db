2de674f89936e0e17af862dfd0a9b1955fad1550
Steve Arnett <steve@sheetmusicplus.com>
2016-09-23 22:37:51 -0700

    remove api wsgi

--- start.sh
+++ start.sh
@@ -12,4 +12,3 @@ kill $(ps aux | grep '[g]unicorn' | awk '{print $2}')
 sudo /etc/init.d/nginx restart
 
 gunicorn --bind 0.0.0.0:5000 --workers 16 wsgi &
-gunicorn --bind 0.0.0.0:5050 --workers 16 wsgi_api &
--- wsgi_api.py
+++
@@ -1,6 +0,0 @@
-from app import api
-
-application = app
-
-if __name__ == "__main__":
-    application.run()
